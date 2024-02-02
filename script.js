09db881316b3233bc824e31515c80ca26080958e
gonzagaa <iigust4vo@gmail.com>
2024-02-02 11:34:32 -0300

    update

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ function toggleMode() {
     const img = document.querySelector('#profile img')
 
     if(html.classList.contains('light')) {
-        img.setAttribute('src', './assets/avatar-light.png')
-        img.setAttribute('alt', 'Foto de Gustavo Sorrindo Light Mode')
+        img.setAttribute('src', './assets/foto gg 2.webp')
+        img.setAttribute('alt', 'Foto de Gustavo Gonzaga Light Mode')
     } else {
-        img.setAttribute('src', './assets/avatar.png')
-        img.setAttribute('alt', 'Foto de Gustavo Sorrindo Dark Mode')
+        img.setAttribute('src', './assets/foto gg 2.webp')
+        img.setAttribute('alt', 'Foto de Gustavo Gonzaga Dark Mode')
     }
-
 }
+
+ScrollReveal().reveal('ul li', { distance: '400px', origin: 'left', duration: 500, easing: 'ease-in-out'});
+ScrollReveal().reveal('ul li.right', { distance: '400px', origin: 'right', duration: 500, easing: 'ease-in-out'});
+
+ScrollReveal().reveal('#profile', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out'});
+
+ScrollReveal().reveal('.links-sociais, #social-links, footer', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out'});
+
+ScrollReveal().reveal('#switch', { distance: '0px', opacity: 0, duration: 1000, easing: 'ease-in-out'});
+
+
+
